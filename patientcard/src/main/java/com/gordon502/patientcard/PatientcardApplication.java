f5f03aa35c1ec0b3b80847a96974d443fbd1201c
gordon502 <gordonarek@gmail.com>
2020-05-12 09:42:54 +0200

    Add Spring boot app skeleton

---
+++ patientcard/src/main/java/com/gordon502/patientcard/PatientcardApplication.java
@@ -0,0 +1,13 @@
+package com.gordon502.patientcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientcardApplication.class, args);
+	}
+
+}
---
+++ patientcard/src/test/java/com/gordon502/patientcard/PatientcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gordon502.patientcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
