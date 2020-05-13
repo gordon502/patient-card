637ba17cd7358d210c0ea98bf3b1629a91eb86fe
gordon502 <gordonarek@gmail.com>
2020-05-13 13:23:00 +0200

    Front-end adjustments

--- patientcard/src/main/resources/static/scripts/functions.js
+++ patientcard/src/main/resources/static/scripts/functions.js
@@ -1,27 +1,89 @@
-const patientsTableLegend = '<tr>' +
-    '                    <th>ID</th>' +
-    '                    <th>Imię i nazwisko</th>' +
-    '                    <th>Płeć</th>' +
-    '                    <th>Data urodzenia</th>' +
-    '                    <th>Adres zamieszkania</th>' +
-    '                    <th>Nr telefonu</th>' +
-    '                </tr>';
+const patientsTableLegend = `    <tr>
+                                    <th onclick="sortTable(0)">ID</th>
+                                    <th onclick="sortTable(1)">Imię i nazwisko</th>
+                                    <th onclick="sortTable(2)">Płeć</th>
+                                    <th onclick="sortTable(3)">Data urodzenia</th>
+                                    <th onclick="sortTable(4)">Adres zamieszkania</th>
+                                    <th onclick="sortTable(5)">Nr telefonu</th>
+                                    <th>Szczegóły</th>
+                                </tr>`;
 
 function fillTable() {
     fetch('http://localhost:8081/Patients')
         .then(response => response.json())
         .then(data => {
-            document.getElementById("patientsTable").innerHTML = patientsTableLegend;
+            var tableStructure = '';
             for (var i = 0; i < data.length; i++) {
                 var p = data[i];
 
-                document.getElementById("patientsTable")
-                    .innerHTML += '<tr><td>' + p["id"] +
-                    '</td><td>' + p["name"] +
-                    '</td><td>' + p["gender"] +
-                    '</td><td>' + p["birthDate"] +
-                    '</td><td>' + p["address"] +
-                    '</td><td>' + p["telecom"] + '</td></tr>';
+                tableStructure +=
+                        `<tr>
+                            <td id="id${i}">${p["id"]}</td>
+                            <td>${p["name"]}</td>
+                            <td>${p["gender"]}</td>
+                            <td>${p["birthDate"]}</td>
+                            <td>${p["address"]}</td>
+                            <td>${p["telecom"]}</td>
+                            <td><button id="btn${i}">Przejdź</button></td>  
+                         </tr>`;
             }
-        })
+            document.getElementById("rowToInsert").innerHTML = tableStructure;
+            sortTable(1);
+        });
+    return true;
+}
+
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("patientsTable");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /* Check if the two rows should switch place,
+            based on the direction, asc or desc: */
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount ++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
 }
